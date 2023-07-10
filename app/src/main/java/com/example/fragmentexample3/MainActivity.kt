691bf148e78f482d3b708505382cc39b532247e5
Willy <guillecg13@gmail.com>
2023-07-10 19:57:15 +0200

    Added: activity xml and kotlin class

--- app/src/main/java/com/example/fragmentexample3/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexample3/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.fragmentexample3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentexample3.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewFragment.DatoDevuelto {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnChangeFragment.setOnClickListener {
+            showFragment()
+        }
+    }
+
+    private fun showFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        val fragment = NewFragment()
+
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun datoActualizado(dato: String) {
+        binding.tvInformation.text = dato
     }
 }
